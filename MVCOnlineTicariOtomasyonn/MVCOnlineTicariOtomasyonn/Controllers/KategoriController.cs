4b385a30006280b54b8d7e528e617f9d07a5bee5
Mferik <mfatiherikk@gmail.com>
2023-10-17 20:54:04 +0300

    databaseden kategori verilerini layouta gönderme

--- MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/KategoriController.cs
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models.Sınıflar;
 
 namespace MVCOnlineTicariOtomasyonn.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
         }
     }
 }
