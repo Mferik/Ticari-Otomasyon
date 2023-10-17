8664ef0da572302ffa1be9bdc5069d2da0d6159e
Mferik <mfatiherikk@gmail.com>
2023-10-17 11:58:41 +0300

    Ürün ve Kategori sınıfları oluşturuldu

---
+++ MVCOnlineTicariOtomasyon/WebApplication1/Models/Sınıflar/Kategori.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.Sınıflar
+{
+    public class Kategori
+    {
+        [Key]
+        public int KategoriID { get; set; }
+        public string KategoriAd { get; set; }
+    }
+}
---
+++ MVCOnlineTicariOtomasyon/WebApplication1/Models/Sınıflar/Ürün.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models.Sınıflar
+{
+    public class Ürün
+    {
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+        public string UrunGorsel { get; set; }
+
+    }
+}
