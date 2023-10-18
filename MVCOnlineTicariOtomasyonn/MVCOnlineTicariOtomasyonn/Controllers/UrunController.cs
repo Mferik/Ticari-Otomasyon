f208fbf9b6bb8a33832ffb81f8300b0d598b33bd
Mferik <mfatiherikk@gmail.com>
2023-10-18 19:36:20 +0300

    Ürünler index sayfası oluşturuldu

---
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/UrunController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Sınıflar;
+
+namespace MVCOnlineTicariOtomasyonn.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult Index()
+            
+        {
+            var urunler = c.Ürüns.ToList();
+            return View(urunler);
+        }
+    }
+}
