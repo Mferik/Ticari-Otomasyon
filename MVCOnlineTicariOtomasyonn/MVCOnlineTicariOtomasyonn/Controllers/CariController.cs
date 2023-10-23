03261857df5be055b4fe1ea898cac40a3b7a207c
Mferik <mfatiherikk@gmail.com>
2023-10-23 13:23:18 +0300

    Cari Sayfası Tasarım Tablo vs.

---
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/CariController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Sınıflar;
+namespace MVCOnlineTicariOtomasyonn.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Carilers.ToList();
+            return View(degerler);
+        }
+    }
+}
