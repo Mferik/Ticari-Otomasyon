c9ab7bd4cfc53e69e7604139b021c95a27c1db66
Mferik <mfatiherikk@gmail.com>
2023-10-23 16:48:31 +0300

    Satış Hareket Index Sayfası + YeniSatis baslangıç

---
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Sınıflar;
+
+namespace MVCOnlineTicariOtomasyonn.Controllers
+{
+    public class SatisController : Controller
+    {
+        Context c = new Context();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
