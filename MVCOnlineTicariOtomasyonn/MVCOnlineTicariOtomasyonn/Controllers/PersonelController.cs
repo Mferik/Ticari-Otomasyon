036ab83be59e9bee1b779287fc0aaa489ab50bff
Mferik <mfatiherikk@gmail.com>
2023-10-23 15:11:21 +0300

    Personel Controller and Index

---
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/PersonelController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Sınıflar;
+namespace MVCOnlineTicariOtomasyonn.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Index()
+        {
+            Context c = new Context();
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+    }
+}
