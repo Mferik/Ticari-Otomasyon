a2c283f339516e21e8ec2302e715dcc94eefc52f
Mferik <mfatiherikk@gmail.com>
2023-10-23 17:35:23 +0300

    Yeni Satış İşlemi

--- MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/SatisController.cs
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/SatisController.cs
@@ -19,11 +19,45 @@ namespace MVCOnlineTicariOtomasyonn.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            List<SelectListItem> deger1 = (from x in c.Ürüns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.UrunID.ToString(),
+                                           }).ToList();
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.CariId.ToString(),
+                                           }).ToList();
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.PersonelId.ToString(),
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         [HttpPost]
         public ActionResult YeniSatis(SatisHareket s)
         {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            if (s.Personel != null)
+            {
+                s.Personel = c.Personels.Find(s.Personel.PersonelId);
+            }
+            if (s.Ürün != null)
+            {
+                s.Ürün = c.Ürüns.Find(s.Ürün.UrunID);
+            }
+            if (s.Cariler != null)
+            {
+                s.Cariler = c.Carilers.Find(s.Cariler.CariId);
+            }
             c.SatisHarekets.Add(s);
             c.SaveChanges();
             return RedirectToAction("Index");
