433603b22c3916bcc9b21c52f577ec65fd155fec
Mferik <mfatiherikk@gmail.com>
2023-10-25 22:04:42 +0300

    İstatistik sayfaları tüm LINQ Sorguları yazıldı

--- MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/İstatistikController.cs
+++ MVCOnlineTicariOtomasyonn/MVCOnlineTicariOtomasyonn/Controllers/İstatistikController.cs
@@ -13,21 +13,38 @@ namespace MVCOnlineTicariOtomasyonn.Controllers
         public ActionResult Index()
         {
             var deger1 = c.Carilers.Count().ToString();
-            ViewBag.d1=deger1;
+            ViewBag.d1 = deger1;
             var deger2 = c.Ürüns.Count().ToString();
             ViewBag.d2 = deger2;
             var deger3 = c.Personels.Count().ToString();
             ViewBag.d3 = deger3;
             var deger4 = c.Kategoris.Count().ToString();
             ViewBag.d4 = deger4;
-            var deger5 = c.Ürüns.Sum(x=>x.Stok).ToString();
+            var deger5 = c.Ürüns.Sum(x => x.Stok).ToString();
             ViewBag.d5 = deger5;
             var deger6 = (from x in c.Ürüns select x.Marka).Distinct().Count().ToString();
             ViewBag.d6 = deger6;
-            var deger7 = c.Ürüns.Count(x=>x.Stok<=20).ToString();
+            var deger7 = c.Ürüns.Count(x => x.Stok <= 20).ToString();
             ViewBag.d7 = deger7;
             var deger8 = (from x in c.Ürüns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
             ViewBag.d8 = deger8;
+            var deger9 = (from x in c.Ürüns orderby x.SatisFiyat ascending select x.UrunAd).FirstOrDefault();
+            ViewBag.d9 = deger9;
+            var deger10 = c.Ürüns.Count(x => x.UrunAd == "Buzdolabı").ToString();
+            ViewBag.d10 = deger10;
+            var deger11 = c.Ürüns.Count(x => x.UrunAd == "Laptop").ToString();
+            ViewBag.d11 = deger11;
+            var deger12 = c.Ürüns.GroupBy(x => x.Marka).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault();
+            ViewBag.d12= deger12;
+            var deger13 =c.Ürüns.Where(u=>u.UrunID == (c.SatisHarekets.GroupBy(x => x.UrunId).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault())).Select(k=>k.UrunAd).FirstOrDefault();
+            ViewBag.d13= deger13;
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d14 = deger14;
+            DateTime bugün = DateTime.Today;
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == bugün).ToString();
+            ViewBag.d15 = deger15;
+            var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugün).Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d16 = deger16;
             return View();
         }
     }
